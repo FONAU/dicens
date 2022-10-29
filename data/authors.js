8fccae3ae39992c1e74c45b5d04c220bada7ce92
FONAU <fonau.io@gmail.com>
2022-10-29 19:32:56 +0200

    Initial push

---
+++ data/authors.js
@@ -0,0 +1,514 @@
+const AUTHORS = {
+
+    "A": [
+        {
+          "name": "Abrantes, Duquesa de ",
+          "info": "Escritora francesa",
+          "birth": "1783",
+          "demise": "1834"
+        },
+        {
+          "name": "Abzug, Bella ",
+          "info": "Política estadounidense",
+          "birth": "1920",
+          "demise": "1998"
+        },
+        {
+          "name": "Acton, Lord ",
+          "info": "Historiador inglés",
+          "birth": "1834",
+          "demise": "1902"
+        },
+        {
+          "name": "Adams, Henry ",
+          "info": "Escritor y historiador estadounidense",
+          "birth": "1838",
+          "demise": "1918"
+        },
+        {
+          "name": "Adams, Douglas ",
+          "info": "Escritor británico",
+          "birth": "1952",
+          "demise": "2001"
+        },
+        {
+          "name": "Addison, Joseph ",
+          "info": "Ensayista, poeta y dramaturgo inglés",
+          "birth": "1672",
+          "demise": "1719"
+        },
+        {
+          "name": "Adenauer, Konrad ",
+          "info": "Político alemán",
+          "birth": "1876",
+          "demise": "1967"
+        },
+        {
+          "name": "Adler, Alfred ",
+          "info": "Psicólogo y psiquiatra austriaco",
+          "birth": "1870",
+          "demise": "1937"
+        },
+        {
+          "name": "Adorno, Theodor W. ",
+          "info": "Filósofo alemán",
+          "birth": "1903",
+          "demise": "1969"
+        },
+        {
+          "name": "Agatón ",
+          "info": "Dramaturgo griego",
+          "birth": "448AC",
+          "demise": "401AC"
+        },
+        {
+          "name": "Aguiló, Mariano ",
+          "info": "Poeta español",
+          "birth": "1825",
+          "demise": "1897"
+        },
+        {
+          "name": "Alain ",
+          "info": "Filósofo y ensayista francés",
+          "birth": "1868",
+          "demise": "1951"
+        },
+        {
+          "name": "Alatriste, Sealtiel ",
+          "info": "Editor y escritor mexicano",
+          "birth": "1949",
+          "demise": "?"
+        },
+        {
+          "name": "Alberti, Rafael ",
+          "info": "Poeta y dramaturgo español",
+          "birth": "1902",
+          "demise": "1999"
+        },
+        {
+          "name": "Alberti, Leon Battista ",
+          "info": "Arquitecto y escritor italiano",
+          "birth": "1404",
+          "demise": "1472"
+        },
+        {
+          "name": "Alcott, Amos Bronson ",
+          "info": "Filósofo y profesor estadounidense",
+          "birth": "1799",
+          "demise": "1888"
+        },
+        {
+          "name": "Alcott, Louise May ",
+          "info": "Escritora estadounidense",
+          "birth": "1832",
+          "demise": "1888"
+        },
+        {
+          "name": "Aldecoa, Josefina ",
+          "info": "Escritora y pedagoga española",
+          "birth": "1926",
+          "demise": "2011"
+        },
+        {
+          "name": "Alemán, Mateo ",
+          "info": "Novelista español",
+          "birth": "1547",
+          "demise": "1613"
+        },
+        {
+          "name": "Alexander, Samuel ",
+          "info": "Filósofo británico",
+          "birth": "1859",
+          "demise": "1938"
+        },
+        {
+          "name": "Alfieri, Vittorio ",
+          "info": "Dramaturgo y poeta italiano",
+          "birth": "1749",
+          "demise": "1803"
+        },
+        {
+          "name": "Alfonso V el Magnánimo ",
+          "info": "Rey de Aragón",
+          "birth": "1394",
+          "demise": "1458"
+        },
+        {
+          "name": "Alfonso X el Sabio ",
+          "info": "Rey de Castilla y León",
+          "birth": "1221",
+          "demise": "1284"
+        },
+        {
+          "name": "Alighieri, Dante ",
+          "info": "Escritor y filósofo italiano",
+          "birth": "1265",
+          "demise": "1321"
+        },
+        {
+          "name": "Allais, Alphonse ",
+          "info": "Escritor francés",
+          "birth": "1855",
+          "demise": "1905"
+        },
+        {
+          "name": "Allen, Fred ",
+          "info": "Humorista estadounidense",
+          "birth": "1894",
+          "demise": "1956"
+        },
+        {
+          "name": "Allen, Woody ",
+          "info": "Actor, director y escritor estadounidense",
+          "birth": "1935",
+          "demise": "?"
+        },
+        {
+          "name": "Allende, Isabel ",
+          "info": "Escritora chilena",
+          "birth": "1942",
+          "demise": "?"
+        },
+        {
+          "name": "Almazán, Marco Aurelio ",
+          "info": "Escritor y diplomático mexicano",
+          "birth": "1922",
+          "demise": "1991"
+        },
+        {
+          "name": "Almodovar, Pedro ",
+          "info": "Director de cine español",
+          "birth": "1950",
+          "demise": "?"
+        },
+        {
+          "name": "Amicis, Edmundo de ",
+          "info": "Escritor italiano",
+          "birth": "1846",
+          "demise": "1908"
+        },
+        {
+          "name": "Amiel, Henry F. ",
+          "info": "Escritor suizo",
+          "birth": "1821",
+          "demise": "1881"
+        },
+        {
+          "name": "Ana de Austria",
+          "birth": ""
+        },
+        {
+          "name": "Anacarsis ",
+          "info": "Filósofo escita",
+          "birth": "s. VII AC",
+          "demise": "s. VII AC"
+        },
+        {
+          "name": "Anaxágoras ",
+          "info": "Filósofo griego",
+          "birth": "500 A.C.",
+          "demise": "428 A.C."
+        },
+        {
+          "name": "Anderson, Robert",
+          "birth": ""
+        },
+        {
+          "name": "Andréiev, Leonid ",
+          "info": "Escritor ruso",
+          "birth": "1871",
+          "demise": "1919"
+        },
+        {
+          "name": "Andreotti, Giulio ",
+          "info": "Político italiano",
+          "birth": "1919",
+          "demise": "?"
+        },
+        {
+          "name": "Anguita, Julio ",
+          "info": "Político español",
+          "birth": "1941",
+          "demise": "?"
+        },
+        {
+          "name": "Annan, Kofi ",
+          "info": "Secretario General de las Naciones Unidas",
+          "birth": "1938",
+          "demise": "?"
+        },
+        {
+          "name": "Annunzio, Gabriele d' ",
+          "info": "Escritor italiano",
+          "birth": "1863",
+          "demise": "1938"
+        },
+        {
+          "name": "Anouilh, Jean ",
+          "info": "Escritor y dramaturgo francés",
+          "birth": "1910",
+          "demise": "1987"
+        },
+        {
+          "name": "Antonioni, Michelangelo ",
+          "info": "Cineasta y escritor italiano",
+          "birth": "1912",
+          "demise": "2007"
+        },
+        {
+          "name": "Apollinarie Fée, Antonie L. ",
+          "info": "Farmacéutico y naturalista francés",
+          "birth": "1789",
+          "demise": "1874"
+        },
+        {
+          "name": "Apuleyo, Lucio ",
+          "info": "Escritor italiano",
+          "birth": "123",
+          "demise": "180"
+        },
+        {
+          "name": "Aquino, Santo Tomás de ",
+          "info": "Filósofo y teólogo",
+          "birth": "1224",
+          "demise": "1274"
+        },
+        {
+          "name": "Aragonés, Luis ",
+          "info": "Jugador y entrenador de fútbol español",
+          "birth": "1938",
+          "demise": "2014"
+        },
+        {
+          "name": "Arenal, Concepción ",
+          "info": "Escritora y socióloga española",
+          "birth": "1820",
+          "demise": "1893"
+        },
+        {
+          "name": "Arendt, Hannah ",
+          "info": "Filósofa y teórica política alemana",
+          "birth": "1906",
+          "demise": "1975"
+        },
+        {
+          "name": "Aristófanes ",
+          "info": "Dramaturgo griego",
+          "birth": "444 AC",
+          "demise": "385 AC"
+        },
+        {
+          "name": "Aristóteles ",
+          "info": "Filósofo griego",
+          "birth": "384 AC",
+          "demise": "322 AC"
+        },
+        {
+          "name": "Armiñán, Jaime de ",
+          "info": "Escritor español",
+          "birth": "1935",
+          "demise": "?"
+        },
+        {
+          "name": "Arquímedes",
+          "birth": ""
+        },
+        {
+          "name": "Arrabal, Fernando ",
+          "info": "Dramaturgo, poeta y novelista español",
+          "birth": "1932",
+          "demise": "?"
+        },
+        {
+          "name": "Arréat, Jean Lucien ",
+          "info": "Escritor y filósofo francés",
+          "birth": "1841",
+          "demise": "1922"
+        },
+        {
+          "name": "Ashbery, John ",
+          "info": "Poeta estadounidense",
+          "birth": "1927",
+          "demise": "?"
+        },
+        {
+          "name": "Asimov, Isaac ",
+          "info": "Escritor y bioquímico estadounidense",
+          "birth": "1920",
+          "demise": "1992"
+        },
+        {
+          "name": "Auden, W. H. ",
+          "info": "Poeta inglés",
+          "birth": "1907",
+          "demise": "1973"
+        },
+        {
+          "name": "Auerbach, Berthold",
+          "birth": ""
+        },
+        {
+          "name": "Auguez, Paul ",
+          "birth": "Escritor francés."
+        },
+        {
+          "name": "Aulo Persio ",
+          "info": "Poeta satírico latino",
+          "birth": "34",
+          "demise": "62"
+        },
+        {
+          "name": "Austen, Jane ",
+          "info": "Escritora británica",
+          "birth": "1775",
+          "demise": "1817"
+        },
+        {
+          "name": "Auster, Paul ",
+          "info": "Novelista y poeta estadounidense",
+          "birth": "1947",
+          "demise": "?"
+        },
+        {
+          "name": "Avellaneda, Nicolás de ",
+          "info": "Político y periodista argentino",
+          "birth": "1837",
+          "demise": "1885"
+        },
+        {
+          "name": "Averroes ",
+          "info": "Filósofo y médico hispanoárabe",
+          "birth": "1126",
+          "demise": "1198"
+        },
+        {
+          "name": "Avivar, Pilar ",
+          "birth": "Profesora de arte."
+        },
+        {
+          "name": "Ayala, Francisco ",
+          "info": "Escritor español",
+          "birth": "1906",
+          "demise": "2009"
+        },
+        {
+          "name": "Aymé, Marcel ",
+          "info": "Narrador y dramaturgo francés cuya obra combina lo",
+          "birth": "1902",
+          "demise": "1967"
+        },
+        {
+          "name": "Azaña, Manuel ",
+          "info": "Político y escritor español",
+          "birth": "1880",
+          "demise": "1940"
+        },
+        {
+          "name": "Azorín ",
+          "info": "Ensayista, novelista y autor de teatro español",
+          "birth": "1873",
+          "demise": "1967"
+        }
+    ],
+
+    "B": {
+
+    },
+
+    "C": {
+
+    },
+
+    "D": {
+
+    },
+
+    "E": {
+
+    },
+
+    "F": {
+
+    },
+
+    "G": {
+
+    },
+
+    "H": {
+
+    },
+
+    "I": {
+
+    },
+
+    "J": {
+
+    },
+
+    "K": {
+
+    },
+
+    "L": {
+
+    },
+
+    "M": {
+
+    },
+
+    "N": {
+
+    },
+    
+    "O": {
+
+    },
+
+    "P": {
+
+    },
+
+    "Q": {
+
+    },
+
+    "R": {
+
+    },
+
+    "S": {
+
+    },
+
+    "T": {
+
+    },
+
+    "U": {
+
+    },
+
+    "V": {
+
+    },
+
+    "W": {
+
+    },
+
+    "X": {
+
+    },
+
+    "Y": {
+
+    },
+
+    "Z": {
+
+    }
+
+};
